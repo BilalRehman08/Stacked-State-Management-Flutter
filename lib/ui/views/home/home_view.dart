3b501b17d3c5fd8e14248047bcf262a6af981898
Circaadia <circaadia@Circaadias-MacBook-Pro.local>
2021-09-28 18:35:44 +0500

    Navigation

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -11,10 +11,24 @@ class HomeView extends StatelessWidget {
     return ViewModelBuilder<HomeViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: Center(
-          child: 
-                  Text("${model.count}"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
+                child: Center(child: Text("${model.count}",))),
+
+                ElevatedButton(onPressed: (){
+                    model.updateCounter();
+                }, child: const Text("Increament 1")),
+                ElevatedButton(onPressed: (){
+                    model.doubleCounter();  
+                }, child: const Text("Multiply by 2")),
+              ],
             ),
-            floatingActionButton: FloatingActionButton(onPressed: (){model.updateCounter();},),
+        ),
           ),
       viewModelBuilder: () => HomeViewModel(),
     );
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -4,8 +4,16 @@ class HomeViewModel extends BaseViewModel {
    int count = 0;
 
   //  int get counter => count;
+void doubleCounter (){ 
+    count=count*2;
+    notifyListeners();  
+}
+
+
   void updateCounter (){ 
     count++;
     notifyListeners();  
 }
+
+  
 }
